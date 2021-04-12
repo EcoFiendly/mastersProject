f31a78377f18928cdf5f992f140b545ac14d9e1e
Yewshen Lim <y.lim20@imperial.ac.uk>
2021-04-12 14:31:11 +0100

    Remove code for individual categories

--- Code/rat/runRatModOpt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#PBS -l walltime=1:00:00
-#PBS -l select=1:ncpus=8:mem=16gb
-module load anaconda3/personal
-echo "About to optimize model"
-python3 $HOME/Code/rat/ratModOptHPCarray.py
-echo "Finished optimization"
-# end of file
--- Code/rng/runRngTokPrep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#PBS -l walltime=1:00:00
-#PBS -l select=1:ncpus=8:mem=12gb
-module load anaconda3/personal
-echo "Tokenization"
-python3 $HOME/Code/rng/rngTokenPrepHPC.py
-echo "Done"
-# end of file
